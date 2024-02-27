0100d44639095e2e020b7ad3e89bb0ac499d56e7
abdalla rahmah <abdallarahmah.dev@gmail.com>
2024-02-28 03:43:33 +0400

    add project src code files with code
    - add index.html file.
    - add scripts folder includes index.js file.
    - add styles folder includes main.css file.

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,30 @@
-let titles = document.querySelectorAll(`.content__q`);
+const questionList = document.querySelectorAll(`.content .question`);
 
-let arrows = document.querySelectorAll(`.content__arrow`);
-
-let answers = document.querySelectorAll(`.answer`);
-
-var visible = (content) => {
-  titles.forEach((title) => {
-    title.style.color = "hsl(237, 12%, 33%)";
-    title.style.fontWeight = `normal`;
+questionList.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    listController(e.target);
   });
+});
 
-  document.getElementById(
-    content
-  ).children[0].style.color = `hsl(238, 29%, 16%)`;
+function listController(item) {
+  let status = item.getAttribute("aria-expanded");
 
-  document.getElementById(
-    content
-  ).children[0].style.fontWeight = `bold`;
+  if (status == "false") {
+    closeLists(questionList);
+    openList(item);
+  } else {
+    closeLists(questionList);
+  }
+}
 
-  arrows.forEach((a) => {
-    a.style.transform = "rotate(0deg)";
+function closeLists(list = []) {
+  list.forEach((item) => {
+    item.parentElement.classList.remove("active");
+    item.setAttribute("aria-expanded", "false");
   });
+}
 
-  document.getElementById(content).children[1].style.transform =
-    "rotatex(180deg)";
-
-  answers.forEach((answer) => {
-    answer.style.display = "none";
-  });
-
-  document.getElementById(content).children[2].style.display = "inline";
-};
+function openList(item) {
+  item.parentElement.classList.add("active");
+  item.setAttribute("aria-expanded", "true");
+}
